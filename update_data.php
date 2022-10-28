a7c218f7b1954f2069ba735019f198052299fa08
alyanabilah <116285415+alyanabilah@users.noreply.github.com>
2022-10-28 20:46:56 +0700

    update data

--- update_data.php
+++ update_data.php
@@ -1,10 +1,7 @@
 <?php
 require 'database.php';
-$name = $_POST['name'];
-$password = sha1($_POST['password']);
-$role = $_POST['role'];
-$date_modified = date('d/m/Y', time());
-$email1 = $_POST['email'];
+
+$id = $_GET['id'];
 
 $sql1 = "UPDATE products SET name='$name',
 description='$desc',
@@ -14,7 +11,7 @@ modified = sysdate()
 WHERE id='$id'";
 
 if (mysqli_query($conn, $sql1)) {
-    echo "data berhasil dimasukkan ke database";
+    //echo "data berhasil dimasukkan ke database";
     header('Location: read_data.php');
 } else {
     echo "gagal memasukkan data: " . mysqli_error($conn);
